db52cfd7b1fc3c6515d654f511a9eeaf3071f7b7
dannyhma <dannyhimawan089@gmail.com>
2024-03-13 14:34:33 +0700

    methods

--- template/computedProperty.js
+++ template/computedProperty.js
@@ -6,13 +6,41 @@ const app = createApp({
     };
   },
   computed: {
-    slugTize: function () {
-      return this.slugText
-        .toLowerCase()
-        .replace(/[^\w]+/g, '-')
-        .replace(/-+/g, '-')
-        .replace(/^-|-$/g, '');
+    now: function () {
+      var date = new Date();
+      return (
+        String(date.getHours()) +
+        ':' +
+        String(date.getMinutes()) +
+        ':' +
+        String(date.getSeconds())
+      );
     },
+
+    slugTize: function () {
+      return (
+        this.slugText
+          .toLowerCase()
+          .replace(/[^\w]+/g, '-')
+          .replace(/-+/g, '-')
+          .replace(/^-|-$/g, '') +
+        '-' +
+        // this.now()
+        this.now
+      );
+    },
+  },
+  methods: {
+    // now: function () {
+    //   var date = new Date();
+    //   return (
+    //     String(date.getHours()) +
+    //     ':' +
+    //     String(date.getMinutes()) +
+    //     ':' +
+    //     String(date.getSeconds())
+    //   );
+    // },
   },
 });
 
