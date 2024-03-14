0660609b9339a38d7865760ce83918a517a64dee
dannyhma <dannyhimawan089@gmail.com>
2024-03-14 08:25:14 +0700

    styling

--- basic/app.js
+++ basic/app.js
@@ -1,57 +1,16 @@
 const { createApp } = Vue;
-// const dataProduct = {
-//   maximum: 50,
-//   products: [
-//     {
-//       id: '01',
-//       image: 'https://unsplash.it/600/350.jpg?image=436',
-//       name: 'Vue.js',
-//       image_title: 'Vue.js',
-//       description: 'Vue.js adalah sebuah framework JavaScript yang digunakan untuk membangun antarmuka pengguna (UI) yang interaktif dan reaktif. Diciptakan oleh Evan You pada tahun 2014, Vue.js telah menjadi salah satu alat yang populer dalam pengembangan web karena kemudahannya dalam dipelajari dan digunakan.',
-//       price: '49',
-//     },
-//     {
-//       id: '02',
-//       image: 'https://unsplash.it/600/350.jpg?image=506',
-//       name: 'Vue.js',
-//       image_title: 'Vue.js',
-//       description: 'Vue.js adalah sebuah framework JavaScript yang digunakan untuk membangun antarmuka pengguna (UI) yang interaktif dan reaktif. Diciptakan oleh Evan You pada tahun 2014, Vue.js telah menjadi salah satu alat yang populer dalam pengembangan web karena kemudahannya dalam dipelajari dan digunakan.',
-//       price: '60',
-//     },
-//     {
-//       id: '03',
-//       image: 'https://unsplash.it/600/350.jpg?image=146',
-//       name: 'Vue.js',
-//       image_title: 'Vue.js',
-//       description: 'Vue.js adalah sebuah framework JavaScript yang digunakan untuk membangun antarmuka pengguna (UI) yang interaktif dan reaktif. Diciptakan oleh Evan You pada tahun 2014, Vue.js telah menjadi salah satu alat yang populer dalam pengembangan web karena kemudahannya dalam dipelajari dan digunakan.',
-//       price: '99',
-//     },
-//     {
-//       id: '04',
-//       image: 'https://unsplash.it/600/350.jpg?image=345',
-//       name: 'Vue.js',
-//       image_title: 'Vue.js',
-//       description: 'Vue.js adalah sebuah framework JavaScript yang digunakan untuk membangun antarmuka pengguna (UI) yang interaktif dan reaktif. Diciptakan oleh Evan You pada tahun 2014, Vue.js telah menjadi salah satu alat yang populer dalam pengembangan web karena kemudahannya dalam dipelajari dan digunakan.',
-//       price: '30',
-//     },
-//     {
-//       id: '05',
-//       image: 'https://unsplash.it/600/350.jpg?image=236',
-//       name: 'Vue.js',
-//       image_title: 'Vue.js',
-//       description: 'Vue.js adalah sebuah framework JavaScript yang digunakan untuk membangun antarmuka pengguna (UI) yang interaktif dan reaktif. Diciptakan oleh Evan You pada tahun 2014, Vue.js telah menjadi salah satu alat yang populer dalam pengembangan web karena kemudahannya dalam dipelajari dan digunakan.',
-//       price: '10',
-//     },
-//   ],
-// };
 
 const app = createApp({
   data() {
     return {
-      // ...dataProduct,
       maximum: 50,
       products: null,
       cart: [],
+      style: {
+        head: ['h1'],
+        fontSize: 72,
+        sliderStatus: false,
+      },
     };
   },
   mounted() {
@@ -61,6 +20,11 @@ const app = createApp({
         this.products = data;
         console.log(this.products);
       });
+  },
+  computed: {
+    sliderState: function () {
+      return this.style.sliderStatus ? 'd-flex' : 'd-none';
+    },
   },
   methods: {
     addItem: function (product) {
