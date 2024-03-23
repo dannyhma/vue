3464f097a3e348279c5188edc5af365b99d1d8f7
dannyhma <dannyhimawan089@gmail.com>
2024-03-23 16:23:44 +0700

    create component price

--- vite/my-vue-app/src/App.vue
+++ vite/my-vue-app/src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="container mx-auto">
+    <p class="text-4xl font-bold">IDShop</p>
+    <p class="animate__animated animate__fadeInTopRight">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt minima
+      error labore. Magni hic qui dignissimos unde, quia nemo. Suscipit quo rem
+      assumenda, officia sed, pariatur corrupti perferendis eligendi fuga
+      deleniti porro. Reiciendis possimus eius animi repellendus nam. Aliquam
+      dolorem in nam fugit unde quo dolores rerum fuga facere atque?
+    </p>
+    <font-awesome-icon icon="shopping-cart" /> <price :value="4.24" />
+  </div>
 </template>
---
+++ vite/my-vue-app/src/components/Price.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  value: Number,
+  prefix: {
+    type: String,
+    default: "Rp",
+  },
+  precision: {
+    type: Number,
+    default: 1,
+  },
+});
+</script>
+
+<template>
+  <span>{{
+    this.prefix + Number.parseFloat(this.value).toFixed(this.precision)
+  }}</span>
+</template>
