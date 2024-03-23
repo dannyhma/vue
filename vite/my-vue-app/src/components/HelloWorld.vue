3f565920f29444390ad6658a1ffd3333759d4893
dannyhma <dannyhimawan089@gmail.com>
2024-03-23 16:13:24 +0700

    install plugins

--- vite/my-vue-app/src/components/HelloWorld.vue
+++ vite/my-vue-app/src/components/HelloWorld.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { ref } from "vue";
-
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 </script>
 
 <template>
-  <h1 class="container mx-auto text-3xl font-bold text-red-800">
-    Hello world!
-  </h1>
+  <div class="container mx-auto">
+    <p class="text-4xl font-bold">IDShop</p>
+    <p class="animate__animated animate__fadeInTopRight">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt minima
+      error labore. Magni hic qui dignissimos unde, quia nemo. Suscipit quo rem
+      assumenda, officia sed, pariatur corrupti perferendis eligendi fuga
+      deleniti porro. Reiciendis possimus eius animi repellendus nam. Aliquam
+      dolorem in nam fugit unde quo dolores rerum fuga facere atque?
+    </p>
+    <font-awesome-icon icon="shopping-cart" />
+  </div>
 </template>
--- vite/my-vue-app/src/main.js
+++ vite/my-vue-app/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import "./style.css";
+import "animate.css";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart);
 
 createApp(App).mount("#app");
